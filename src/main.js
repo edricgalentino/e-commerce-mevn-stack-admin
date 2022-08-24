6276d5aa51fc51428b5a5ff8f4929b2b149f1c54
edricgalentino <erdrikpertama@gmail.com>
2022-08-24 14:07:08 +0700

    install vue router n create product CRUD page

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import router from "./router";
 import "flowbite";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-
-// import { faBell, faEnvelope, faBox, faArrowTrendUp, faChartLine, faUser, faStore, faUsd, faDollarSign, faChartSimple, faLayerGroup, faMessage, faBriefcase, faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-// import { faGithub } from "@fortawesome/free-brands-svg-icons";
-
 library.add(fas);
-// library.add(faBell, faEnvelope, faGithub, faBox, faArrowTrendUp, faChartLine, faUser, faStore, faUsd, faDollarSign, faChartSimple, faLayerGroup, faMessage, faBriefcase, faCircleExclamation);
 
-createApp(App).component("fa", FontAwesomeIcon).mount("#app");
+// App.use(router);
+
+createApp(App).use(router).component("fa", FontAwesomeIcon).mount("#app");
