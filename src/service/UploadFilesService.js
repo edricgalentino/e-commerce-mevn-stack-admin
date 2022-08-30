674166e99b2ade4aec53a48a363505c6db3a14be
edricgalentino <erdrikpertama@gmail.com>
2022-08-30 16:57:20 +0700

    slicing n integrating

--- src/service/UploadFilesService.js
+++ src/service/UploadFilesService.js
@@ -1,9 +1,19 @@
 import { API } from "../api";
 class UploadFilesService {
-    upload(file) {
+    uploadUser(file) {
         let formData = new FormData();
         formData.append("file", file);
         return API.post("/user/profile-pict", formData, {
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("token"),
+                "Content-Type": "multipart/form-data",
+            },
+        });
+    }
+    uploadProduct(file) {
+        let formData = new FormData();
+        formData.append("file", file);
+        return API.post("/product/product-image", formData, {
             headers: {
                 Authorization: "Bearer " + localStorage.getItem("token"),
                 "Content-Type": "multipart/form-data",
